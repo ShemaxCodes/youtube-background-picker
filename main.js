a8dcf7994717fad6a445f16c01140c773c5e1474
Jashema Panapa <53614792+ShemaxCodes@users.noreply.github.com>
2022-02-23 14:32:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,32 @@
 
+
+document.getElementById('purple').onclick = partyPurple
+document.getElementById('green').onclick = partyGreen
+document.getElementById('blue').onclick = partyBlue
+document.getElementById('yellow').onclick = partyYellow
+
+
+
+function partyPurple() {
+    document.querySelector('body').style.backgroundColor = "rgba(241,63,247,1)";
+
+    document.querySelector('body').style.color = "white";
+}
+
+function partyGreen() {
+    document.querySelector('body').style.backgroundColor = "rgba(0,253,81,1)";
+
+    document.querySelector('body').style.color = "white";
+}
+
+function partyBlue() {
+    document.querySelector('body').style.backgroundColor = "rgb(0, 183, 255)";
+
+    document.querySelector('body').style.color = "white";
+}
+
+function partyYellow() {
+    document.querySelector('body').style.backgroundColor  = "rgb(244, 248, 0)";
+
+    document.querySelector('body').style.color = "white";
+}
